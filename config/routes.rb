08c1dfb8d9a33518c63d5331cf807e9d396c20ac
mari3803 <289700812+c049mari3803->
2026-06-28 15:43:18 +0000

    showのコントローラーまで作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get "bookers/new"
-  get "bookers/index"
-  get "bookers/show"
+  post 'bookers' => 'bookers#create'
+  get 'bookers' => 'bookers#index'
+  get 'bookers/:id' => 'bookers#show'
   get "bookers/edit"
   get '/top' => 'homes#top'
   get "up" => "rails/health#show", as: :rails_health_check
